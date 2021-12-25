47366d757e8fb2b4bf5515160fe9d328dcb04e66
Asad El-Nawajha <asaad2686823@gmail.com>
2021-12-25 22:46:41 +0200

    Babble chat

---
+++ app/src/main/java/com/anawajha/babble/logic/adapter/MessageAdapter.kt
@@ -0,0 +1,66 @@
+package com.anawajha.babble.logic.adapter
+
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Context
+import android.content.res.ColorStateList
+import android.content.res.Resources
+import android.graphics.Color
+import android.view.Gravity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import androidx.recyclerview.widget.RecyclerView
+import com.anawajha.babble.R
+import com.anawajha.babble.databinding.MessageItemBinding
+import com.anawajha.babble.logic.model.Message
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import android.widget.FrameLayout
+import com.anawajha.babble.logic.socket.ImageOperations
+import com.anawajha.babble.ui.Chat
+
+
+class MessageAdapter(var activity: Activity, var messages:ArrayList<Message>):RecyclerView.Adapter<MessageAdapter.MessageViewHolder>() {
+    class MessageViewHolder(binding:MessageItemBinding):RecyclerView.ViewHolder(binding.root){
+        var imageMessage = binding.imgMessage
+        var message = binding.tvMessage
+        var container = binding.loMessageContainer
+    }// MessageViewHolder
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MessageViewHolder {
+        val binding = MessageItemBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return MessageViewHolder(binding)
+    }// onCreateViewHolder
+
+    @SuppressLint("ResourceAsColor")
+    override fun onBindViewHolder(holder: MessageViewHolder, position: Int) {
+        val userId:String? = Firebase.auth.currentUser?.uid
+        userId?.let {
+            if (messages[position].source_id.equals(userId)){
+                holder.container.setBackgroundResource(R.drawable.sender_item)
+                holder.message.setTextColor(R.color.purple)
+                holder.container.gravity = Gravity.END
+                if (messages[position].message is String){
+                    holder.message.text = messages[position].message as String
+                    holder.message.visibility = View.VISIBLE
+                    holder.imageMessage.visibility = View.GONE
+                }else if(messages[position].message is ByteArray){
+                    holder.imageMessage.setImageBitmap(ImageOperations.getImage(messages[position].message as ByteArray))
+                    holder.message.visibility = View.GONE
+                    holder.imageMessage.visibility = View.VISIBLE
+                }
+
+            }else{
+                holder.container.setBackgroundResource(R.drawable.recevier_item)
+                holder.message.setTextColor(R.color.white)
+                holder.container.gravity = Gravity.START
+            }
+        }
+    }// onBindViewHolder
+
+    override fun getItemCount(): Int {
+       return messages.size
+    }// getItemCount
+}// MessageAdapter
--- app/src/main/java/com/anawajha/babble/logic/model/Message.kt
+++ app/src/main/java/com/anawajha/babble/logic/model/Message.kt
@@ -7,13 +7,15 @@ data class Message(var source_id:String, var destination_id:String ,var message:
     fun encode():JSONObject{
         var message= JSONObject()
         message.put("source_id",this.source_id)
-        message.put(destination_id,this.destination_id)
+        message.put("destination_id",this.destination_id)
         message.put("message",this.message)
         return message
     }// encode
 
+companion object{
     fun decode(obj:JSONObject):Message{
         return Message(obj.getString("source_id"),obj.getString("destination_id").toString(),obj.get("message"))
     }// decode
+}// companion object
 
 }// Message class
